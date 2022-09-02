f5e25f6bd33ca9cdcdbd15e9f4552e648b89071d
Lillie Schachter <lillieschachter@gmail.com>
2022-09-01 21:49:26 -0500

    fix tests

--- temp_conversion_test.py
+++ temp_conversion_test.py
@@ -16,32 +16,32 @@ class TestHelloWorld(TestCase):
     def test_convert_100_to_celsius(self, mock_stdout):
         convert_100_to_celsius()
         
-        self.assertEqual('37.77777777777778\nfloat', mock_stdout.getvalue())
+        self.assertEqual('37.77777777777778\nfloat\n', mock_stdout.getvalue())
 
 
     @mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_convert_0_to_celsius(self, mock_stdout):
         convert_0_to_celsius()
         
-        self.assertEqual('-17.77777777777778', mock_stdout.getvalue())
+        self.assertEqual('-17.77777777777778\n', mock_stdout.getvalue())
 
 
     @mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_convert_34_2_to_celsius(self, mock_stdout):
         convert_34_2_to_celsius()
         
-        self.assertEqual('1.2222222222222239', mock_stdout.getvalue())
+        self.assertEqual('1.2222222222222239\n', mock_stdout.getvalue())
 
 
     @mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_convert_5_to_fareinheit(self, mock_stdout):
         convert_5_to_fareinheit()
         
-        self.assertEqual('41.0', mock_stdout.getvalue())
+        self.assertEqual('41.0\n', mock_stdout.getvalue())
 
 
     @mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_hotter_temp(self, mock_stdout):
         hotter_temp()
         
-        self.assertEqual('30.2 degrees celsius', mock_stdout.getvalue())
+        self.assertEqual('30.2 degrees celsius\n', mock_stdout.getvalue())
